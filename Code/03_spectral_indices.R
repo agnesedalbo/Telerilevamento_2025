ea2082cde5c0a39ccc40c18f65284480ec15d20a
agnesedalbo <agnesedalbo@gmail.com>
2025-03-12 17:48:25 +0100

    Update 03_spectral_indices.R

--- Code/03_spectral_indices.R
+++ Code/03_spectral_indices.R
@@ -17,3 +17,44 @@ plot(mato1992)
 im.plotRGB(mato1992, r=1, g=2, b=3)
 im.plotRGB(mato1992, r=2, g=1, b=3)
 im.plotRGB(mato1992, r=2, g=3, b=1)
+
+mato2006 = im.import("matogrosso_ast_2006209_lrg.jpg")
+mato2006 = flip(mato2006)
+plot(mato2006)
+
+im.plotRGB(mato2006, r=1, g=2, b=3)
+
+im.multiframe(1,2)
+im.plotRGB(mato1992, r=1, g=2, b=3)
+im.plotRGB(mato2006, r=1, g=2, b=3)
+
+im.plotRGB(mato1992, r=3, g=2, b=1)
+im.plotRGB(mato2006, r=3, g=2, b=1)
+
+im.multiframe(3,2)
+# NIR ontop of red 
+im.plotRGB(mato1992, r=1, g=2, b=3)
+im.plotRGB(mato2006, r=1, g=2, b=3)
+
+# NIR on top of green 
+im.plotRGB(mato1992, r=2, g=1, b=3)
+im.plotRGB(mato2006, r=2, g=1, b=3)
+
+# NIR on top of blue 
+im.plotRGB(mato1992, r=3, g=2, b=1)
+im.plotRGB(mato2006, r=3, g=2, b=1)
+
+# Exercise: plot only the first layer of mato2006
+dev.off()
+
+plot(mato2006[[1]])
+plot(mato2006[[1]], col=magma(100))
+plot(mato2006[[1]], col=mako(100))
+
+# DVI - Difference Vegetation Index 
+# Tree:          NIR=255 (8 bit), red=0 (8 bit), DVI=NIR-red=255-0=255
+# Stressed Tree: NIR=100 (8 bit), red=30 (8 bit), DVI=NIR-red=100-30=70 
+
+
+
+
