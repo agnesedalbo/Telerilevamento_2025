22c6c6780c3f86d1a48402c6486f60a9d5deb161
agnesedalbo <agnesedalbo@gmail.com>
2025-03-19 17:30:31 +0100

    Update 04_multitemporal analysis.R

--- Code/04_multitemporal analysis.R
+++ Code/04_multitemporal analysis.R
@@ -49,3 +49,15 @@ dev.off()
 png("greenland_output.png")
 plot(gr)
 dev.off()
+
+pdf("greenland_output.pdf")
+plot(gr)
+dev.off()
+
+pdf("difgreen.pdf")
+plot(grdif)
+dev.off()
+
+jpeg("difgreen.jpeg")
+plot(grdif)
+dev.off()
