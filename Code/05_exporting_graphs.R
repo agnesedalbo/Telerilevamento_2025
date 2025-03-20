a710889c70a288311b07ef19f1fa63079d282774
agnesedalbo <agnesedalbo@gmail.com>
2025-03-20 14:25:12 +0100

    Update 05_exporting_graphs.R

--- Code/05_exporting_graphs.R
+++ Code/05_exporting_graphs.R
@@ -1,2 +1,19 @@
 # This code helps exporting graphs to images 
 
+# Exporting data
+setwd("C:\\utente\Downloads") 
+setwd("C://utente/Downloads") 
+setwd("/users/magda/Documents")
+# Windowds users: C://comp/Downloads
+# \
+# setwd("C://nome/Downloads")
+
+getwd()
+
+pdf("output.pdf")
+plot(grdif)
+dev.off()
+
+jpeg("output.jpeg")
+plot(grdif)
+dev.off()
