e2fc559965d27c76353e7b2cb0b66a744d2a75a3
agnesedalbo <agnesedalbo@gmail.com>
2025-07-10 14:50:41 +0200

    Update Esame.R

--- Esame/Esame.R
+++ Esame/Esame.R
@@ -1,1 +1,8 @@
+library(terra) #pacchetto utilizzato per l'importazione di immagini in formato .tif e altre funzioni successive
+library(imageRy) #pacchetto per l'utilizzo della funzione im.plotRGB() per la visualizzazione delle immagini; e le funzioni im.dvi() e im.ndvi()
+library(viridis) #pacchetto per le palette di colori
+library(ggridges) #pacchetto per la creazione di plot ridgeline
+library(ggplot2) #pacchetto per la creazione di grafici a barre
+library(patchwork) #pacchetto per l'unione dei grafici creati con ggplot2
+
 
